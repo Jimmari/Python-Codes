aab86333c510f2c9d8ce21a76c1473fa12ceb41f
JimmariKazungu <jmwhite.con@gmail.com>
2023-11-16 20:37:24 +0300

    Conditions added

---
+++ conditions.py
@@ -0,0 +1,22 @@
+
+#Two types of loops
+#1. For loop
+#2. While loop
+
+# 1. for loop
+# power outage in Kilifi
+
+Kilifi = ['Magarini', 'Shakahola', 'Madunguni', 'Kaloleni'] #no power for these sub-counties
+
+for constituencies in Kilifi:
+        print("No power for:", constituencies)
+        #print(constituencies)
+
+welcome_message = "Welcome To PLP"
+for i in range(2):
+    1, 2, 3, 4, 5
+    print(welcome_message)
+
+    #Assignment
+    #write a for loop that has a start and a stop value for the range
+          
